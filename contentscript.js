d9d7f9740c963cf61d04cef7e2ee0f45541a6d87
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-08-24 15:58:56 +0530

    Show/Hide password on double click.

--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,18 @@
+function dblclickHandler(a){
+    if(a.type=="password")
+        a.setAttribute("type", "text");
+    else if(a.type=="text")
+        a.setAttribute("type", "password");
+}
+
+
 var inputs = document.getElementsByTagName('input');
 var passwordInputs = [];
 for (var i = 0; i < inputs.length; i++) {
     if(inputs[i].type=="password"){
         passwordInputs.push(inputs[i]);
         inputs[i].addEventListener("dblclick", function(){
-            alert();
+            dblclickHandler(this);
         });
     }
 };
